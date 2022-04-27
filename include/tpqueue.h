74b757a053f8a4c23dec84a38b75aa02c9098a62
VladFokin1 <94217312+VladFokin1@users.noreply.github.com>
2022-04-27 19:06:16 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,81 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+  private:
+    struct ITEM {
+        T data;
+        ITEM* next;
+        ITEM* prev;
+    };
+    ITEM* create(const T&) {
+        ITEM* item = new ITEM;
+        item->data = value;
+        item->prev = nullptr;
+        item->next = nullptr;
+        return item;
+    };
+    ITEM* tail;
+    ITEM* head;
+ public:
+    TPQueue():head(nullptr), tail(nullptr) {}
+    void push(const T& value) {
+        ITEM* temp = head;
+        ITEM* item = create(value);
+        while (temp && temp->value.prior >= value.prior) {
+            temp = temp->next;
+        }
+        if (!temp && head) {
+            tail->next = item;
+            item->prev = tail;
+            tail = item;
+        } else if (!temp && !head) {
+            head = tail = item;
+        } else if (!temp->prev) {
+            head->prev = item;
+            item->next = head;
+            head = item;
+        } else {
+            temp->prev->next = item;
+            item->prev = temp->prev;
+            item->next = temp;
+            temp->prev = item;
+        }
+    }
+    T pop() {
+        if (head && tail) {
+            ITEM* temp = head->next;
+            if (temp) {
+                temp->prev = nullptr;
+            }
+            T value = head->value;
+            delete head;
+            head = temp;
+            if (!head) {
+                tail = nullptr;
+            }
+            return value;
+        } else {
+            throw std::string("Error!");
+        }if (head && tail) {
+            ITEM* temp = head->next;
+            if (temp) {
+                temp->prev = nullptr;
+            }
+            T value = head->value;
+            delete head;
+            head = temp;
+            if (!head) {
+                tail = nullptr;
+            }
+            return value;
+        } else {
+            throw std::string("Error!");
+        }
+    }
 };
 
 struct SYM {
